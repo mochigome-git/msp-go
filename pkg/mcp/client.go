8ea363c4f262a01c88ad87111cfaf191ac1b6598
mochigome-git <it@general-i.com.my>
2025-08-11 11:36:16 +0800

    modified:   pkg/mcp/client.go 	modified:   pkg/plc/plc.go

--- pkg/mcp/client.go
+++ pkg/mcp/client.go
@@ -9,6 +9,7 @@ import (
 
 type Client interface {
 	Read(deviceName string, offset, numPoints int64, fx bool) ([]byte, error)
+	Write(deviceName string, offset, numPoints int64, writeData []byte) ([]byte, error)
 	Close() error
 }
 
@@ -95,4 +96,49 @@ func (c *client3E) Close() error {
 		return c.conn.Close()
 	}
 	return nil
+}
+
+// Write is send write command to remote plc by mc protocol
+// deviceName is device code name like 'D' register.
+// offset is device offset addr.
+// writeData is data to write.
+// numPoints is number of write device points.
+// writeData is the data to be written. If writeData is larger than 2*numPoints bytes,
+// data larger than 2*numPoints bytes is ignored.
+func (c *client3E) Write(deviceName string, offset, numPoints int64, writeData []byte) ([]byte, error) {
+	requestStr := c.stn.BuildWriteRequest(deviceName, offset, numPoints, writeData)
+	payload, err := hex.DecodeString(requestStr)
+	if err != nil {
+		return nil, err
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// Create connection if it's not already created
+	if c.conn == nil {
+		conn, err := net.DialTCP("tcp", nil, c.tcpAddr)
+		if err != nil {
+			return nil, err
+		}
+		c.conn = conn
+	}
+
+	// Send message
+	if _, err = c.conn.Write(payload); err != nil {
+		c.conn.Close()
+		c.conn = nil
+		return nil, err
+	}
+
+	// Receive message
+	readBuff := make([]byte, 22) // 22 = fixed response header size
+	readLen, err := c.conn.Read(readBuff)
+	if err != nil {
+		c.conn.Close()
+		c.conn = nil
+		return nil, err
+	}
+
+	return readBuff[:readLen], nil
 }
